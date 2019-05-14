7ab74f1d0e7725cbc5b4df0f4e9b1b5efb022825
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 16:48:57 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length 
+    return collection[i] if yield(collection[i]) # yield(collection[i])  sents collection[i] back to the block, evaluates it, and return it back here as true/false
+    i = i + 1 
+  end
 end
