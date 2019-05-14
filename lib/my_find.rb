d19686de4c78534eece4a0e19fe522a5a2edb433
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 16:45:45 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-    yield(collection[i])
+    yield(collection[i]) # sents collection[i] back to block, evaluate it, and return it back here
     i = i + 1 
   end
 end
